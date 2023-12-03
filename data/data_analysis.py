8c7d3c932e24978caab38276ebcb0dcc7d79e94b
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-12-02 20:57:59 -0500

    Initial analysis

---
+++ data/data_analysis.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+
+
+"""
+This file checks if all the examples in the training set contain the given metaphor words in the exact form or a
+different word form. 
+
+If there are different word forms, then we might have to check for different word forms too. 
+
+"""
+
+
+data=pd.read_csv("train.csv")
+metaphor_id={
+    0: "road",
+1: "candle",
+2: "light",
+3: "spice",
+4: "ride",
+5: "train",
+6: "boat"
+}
+
+contains_target_word=0
+target_word_not_found=0
+total_rows,_=data.shape
+for index,row in data.iterrrows():
+    met_id=row["metaphorID"]
+    text=row["text"]
+    target_word=metaphor_id[met_id]
+    if(text.contains(target_word,case=False)):
+        contains_target_word+=1
+    else:
+        target_word_not_found+=1
+print("Total rows : ",total_rows)
+print("Rows containing target word : ",contains_target_word)
+print("Rows not containing target word : ",target_word_not_found)
+
+
+

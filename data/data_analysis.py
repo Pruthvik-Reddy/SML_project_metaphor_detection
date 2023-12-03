15dac6b3dac2427aa2dcf94726c0fb2cc88b6163
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-12-02 21:01:25 -0500

    Initial analysis

--- data/data_analysis.py
+++ data/data_analysis.py
@@ -33,6 +33,7 @@ for index,row in data.iterrows():
         contains_target_word+=1
     else:
         target_word_not_found+=1
+        print(target_word, text)
 print("Total rows : ",total_rows)
 print("Rows containing target word : ",contains_target_word)
 print("Rows not containing target word : ",target_word_not_found)

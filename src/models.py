9a7092a26cab233758a308075277ecfc594771fa
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-12-05 17:33:46 -0500

    Debugging -1

--- src/models.py
+++ src/models.py
@@ -8,8 +8,9 @@ class BaseBERTClassifier(nn.Module):
         
         self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.dropout = nn.Dropout(p=dropout_prob)
-        self.linear = nn.Linear(self.bert.config.hidden_size, num_classes)
-        self.softmax = nn.Softmax(dim=1)
+        #self.linear = nn.Linear(self.bert.config.hidden_size, num_classes)
+        self.linear = nn.Linear(self.bert.config.hidden_size, 1)
+        #self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input_ids, attention_mask):
         outputs = self.bert(input_ids, attention_mask=attention_mask)
@@ -19,11 +20,10 @@ class BaseBERTClassifier(nn.Module):
         print("Dropped outputs shape : ",dropped_output.shape)
         logits = self.linear(dropped_output)
         print("Logits shape : ",logits.shape)
-        probabilities = self.softmax(logits)
-        print("Probabilities shape : ",probabilities.shape)
+        #probabilities = self.softmax(logits)
+        
 
-
-        return probabilities
+        return logits
 
 
 
